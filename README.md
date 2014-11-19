4ca453fd30fbb9cd3fcf28a51d2f78e0bf1b39a2
David Zurborg <zurborg@cpan.org>
2014-11-19 17:01:28 +0100

    add documentation

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Version 0.100
 
 # DESCRIPTION
 
-...
+This module is an interface to the transformator package of nodejs. See [https://www.npmjs.org/package/transformator](https://www.npmjs.org/package/transformator) for more information about the server.
+
+When it's difficult for perl to interact with various nodejs packages, the transformator protocol allows everyone to interact with an nodejs service. transformator supports a vast range of libraries like jade-lang, sass-lang or coffeescript.
+
+The other way is to invoke each command-line tool as a child process, but this may be very inefficient if such tool need to be called frequently.
 
 # AUTHOR
 
--- lib/Net/NodeTransformator.pm
+++ lib/Net/NodeTransformator.pm
@@ -5,6 +5,7 @@ use warnings;
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
+use POSIX qw(getcwd);
 use Carp;
 
 =head1 NAME
@@ -38,17 +39,37 @@ our $VERSION = '0.100';
 
 =head1 DESCRIPTION
 
-...
+This module is an interface to the transformator package of nodejs. See L<https://www.npmjs.org/package/transformator> for more information about the server.
+
+When it's difficult for perl to interact with various nodejs packages, the transformator protocol allows everyone to interact with an nodejs service. transformator supports a vast range of libraries like jade-lang, sass-lang or coffeescript.
+
+The other way is to invoke each command-line tool as a child process, but this may be very inefficient if such tool need to be called frequently.
 
 =head1 METHODS
 
 =head2 new($hostport)
 
+Set the hostname/port or unix domain socket for connecting to transformator.
+
+	Net::NodeTransformator->new('12345');
+	Net::NodeTransformator->new('localhost:12345');
+	Net::NodeTransformator->new('path/to/unix/domain/socket');	
+
 =cut
 
 sub new {
 	my ($class, $hostport) = @_;
+	if ($hostport !~ m{:}) {
+		if ($hostport =~ m{^\d+$}) {
+			$hostport = "localhost:$hostport";
+		} else {
+			$hostport = "unix/:$hostport";
+		}
+	}
 	my ($host, $port) = parse_hostport($hostport);
+	if ($host eq 'unix/' and $port !~ m{^/}) {
+		$port = getcwd.'/'.$port;
+	}
 	bless {
 		host => $host,
 		port => $port,
@@ -56,6 +77,28 @@ sub new {
 }
 
 =head2 transform_cv(%options)
+
+Connects to transformator and waits for the result asynchronously by using a condition variable.
+
+%options requires for keyworks:
+
+=over 4
+
+=item C<engine> The engine to be used
+
+=item C<input> The input string
+
+=item C<data> (optional) Additional data to be send with. Currently only meaningful for I<jade> engine.
+
+=item C<on_error> A callback subroutine called in case of any error
+
+=back
+
+This method returns a condition variable (L<AnyEvent>->condvar)
+
+	my $cv = $nnt->transform_cv(...);
+
+The result will be pushed to the condvar, so C<$cv->recv> will return the result.
 
 =cut
 
@@ -104,6 +147,8 @@ sub transform_cv($%) {
 
 =head2 transform($engine, $input, $data)
 
+This is the synchronous variant of C<transform_cv>. It croaks on error and can be catched by L<Try::Tiny> for example.
+
 =cut
 
 sub transform($$$;$) {
@@ -122,6 +167,10 @@ sub transform($$$;$) {
 		croak "Something is wrong: $@";
 	}
 }
+
+=head1 SHORTCUT METHODS
+
+This list is incomplete. I will add more methods on request. All methods are hopefully self-describing.
 
 =head2 jade($input, $data)
 
